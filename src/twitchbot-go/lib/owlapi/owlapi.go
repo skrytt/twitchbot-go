82c3c80ba11e03926621224967e56722d51d96a4
skrytt <matt.kemp.1992@gmail.com>
2018-09-23 13:42:56 +0100

    Refactor owlapi code into separate lib

---
+++ src/twitchbot-go/lib/owlapi/owlapi.go
@@ -0,0 +1,148 @@
+package owlapi
+
+// Program must be executed in a directory with a "teams.json" file.
+// JSON schema is based on api.overwatchleague.com/teams , as of 21 September 2018.
+
+// Program outputs information about Overwatch League teams/players to stdout,
+// with some basic markdown formatting for readability.
+
+import (
+    "encoding/json"
+    "io/ioutil"
+    "time"
+)
+
+var _api_data_handle *ApiDataHandle;
+const max_cached_data_age time.Duration = 3600;
+
+// Teams
+
+type Player struct {
+    Flags               []string
+    Team                struct {
+        Id                  int
+        Type                string
+    }
+    Player              struct {
+        Id                  int
+        Erased              bool
+        Handle              string
+        Name                string
+        Type                string
+        FamilyName          string
+        AvailableLanguages  []string
+        AttributesVersion   string
+        Game                string
+        Accounts            []struct {
+            Id              int
+            CompetitorId    int
+            AccountType     string
+            Value           string
+            IsPublic        bool
+        }
+        Headshot            string
+        Nationality         string
+        Attributes          struct {
+            HeroImage           string
+            Heroes              []string
+            Hometown            string
+            Player_Number       int
+            Preferred_Slot      string
+            Role                string
+        }
+        GivenName           string
+        HomeLocation        string
+    }
+}
+
+type Competitor struct {
+    Division            struct {
+        Id                  int
+    }
+    Competitor          struct {
+        Id                  int
+        AbbreviatedName     string
+        Accounts            []struct {
+            CompetitorId        int
+            IsPublic            bool
+            AccountType         string
+            Id                  int
+            Value               string
+        }
+        AddressCountry      string
+        Attributes          struct {
+            Team_Guid           string
+            City                string
+            Manager             string
+            Hero_Image          string
+        }
+        AttributesVersion   string
+        AvailableLanguages  []string
+        Game                string
+        Handle              string
+        HomeLocation        string
+        Icon                string
+        Logo                string
+        Name                string
+        OwlDivision         int
+        Players             []Player
+        PrimaryColor        string
+        SecondaryColor      string
+        SecondaryPhoto      string
+        Type                string
+    }
+}
+
+type Data struct {
+    Id                  int
+    AvailableLanguages  []string
+    Competitors         []Competitor
+    Description         string
+    Game                string
+    Logo                string
+    Name                string
+    OwlDivisions        []struct {
+        Id                  int
+        Name                string
+        String              string
+    }
+    Strings             interface{}
+}
+
+type ApiDataHandle struct {
+    Data        Data
+    Timestamp   time.Time
+}
+
+func GetApiData() (*ApiDataHandle, error) {
+    // We cache the API data to avoid repeatedly downloading it.
+    // Check if our cached version should be updated
+    if _api_data_handle == nil || time.Since(_api_data_handle.Timestamp) > max_cached_data_age {
+        bytes_data, err := ioutil.ReadFile("teams.json")
+        if err != nil {
+            return nil, err
+        }
+
+        new_data := Data{}
+        err = json.Unmarshal(bytes_data, &new_data)
+        if err != nil {
+            return nil, err
+        }
+
+        new_api_data := ApiDataHandle{Data: new_data, Timestamp: time.Now()}
+        _api_data_handle = &new_api_data
+    }
+    return _api_data_handle, nil
+}
+
+func GetCompetitorDataMap() (map[string]Competitor, error) {
+    api_data, err := GetApiData()
+    if err != nil {
+        return make(map[string]Competitor), err
+    }
+    competitor_data_map := make(map[string]Competitor)
+    for _, competitor := range api_data.Data.Competitors {
+        competitor_data_map[competitor.Competitor.Name] = competitor
+    }
+    return competitor_data_map, nil
+}
--- src/twitchbot-go/owlapi.go
+++ src/twitchbot-go/owlapi.go
@@ -7,127 +7,20 @@ package main
 // with some basic markdown formatting for readability.
 
 import (
-    "encoding/json"
     "fmt"
-    "io/ioutil"
     "log"
+    "twitchbot-go/lib/owlapi"
 )
 
-// Teams
-
-type Player struct {
-    Flags               []string
-    Team                struct {
-        Id                  int
-        Type                string
-    }
-    Player              struct {
-        Id                  int
-        Erased              bool
-        Handle              string
-        Name                string
-        Type                string
-        FamilyName          string
-        AvailableLanguages  []string
-        AttributesVersion   string
-        Game                string
-        Accounts            []struct {
-            Id              int
-            CompetitorId    int
-            AccountType     string
-            Value           string
-            IsPublic        bool
-        }
-        Headshot            string
-        Nationality         string
-        Attributes          struct {
-            HeroImage           string
-            Heroes              []string
-            Hometown            string
-            Player_Number       int
-            Preferred_Slot      string
-            Role                string
-        }
-        GivenName           string
-        HomeLocation        string
-    }
-}
-
-type Competitor struct {
-    Division            struct {
-        Id                  int
-    }
-    Competitor          struct {
-        Id                  int
-        AbbreviatedName     string
-        Accounts            []struct {
-            CompetitorId        int
-            IsPublic            bool
-            AccountType         string
-            Id                  int
-            Value               string
-        }
-        AddressCountry      string
-        Attributes          struct {
-            Team_Guid           string
-            City                string
-            Manager             string
-            Hero_Image          string
-        }
-        AttributesVersion   string
-        AvailableLanguages  []string
-        Game                string
-        Handle              string
-        HomeLocation        string
-        Icon                string
-        Logo                string
-        Name                string
-        OwlDivision         int
-        Players             []Player
-        PrimaryColor        string
-        SecondaryColor      string
-        SecondaryPhoto      string
-        Type                string
-    }
-}
-
-type Data struct {
-    Id                  int
-    AvailableLanguages  []string
-    Competitors         []Competitor
-    Description         string
-    Game                string
-    Logo                string
-    Name                string
-    OwlDivisions        []struct {
-        Id                  int
-        Name                string
-        String              string
-    }
-    Strings             interface{}
-}
-
 func main() {
-    var data Data
-
-    raw_data, err := ioutil.ReadFile("teams.json")
+    competitors, err := owlapi.GetCompetitorDataMap()
     if err != nil {
         log.Fatalln(err)
     }
 
-    err = json.Unmarshal(raw_data, &data)
-    if err != nil {
-        log.Fatalln(err)
-    }
-
-    competitors := make(map[string]Competitor)
-    players     := make(map[string]Player)
-
-    for _, competitor := range data.Competitors {
-        competitors[competitor.Competitor.Name] = competitor
+    for _, competitor := range competitors {
         fmt.Printf("**%s:**\n`", competitor.Competitor.Name)
         for _, player := range competitor.Competitor.Players {
-            players[player.Player.Name] = player
             fmt.Printf("%s ", player.Player.Name)
         }
         fmt.Printf("`\n")
